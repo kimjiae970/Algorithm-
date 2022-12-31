0f65fc5cbde102f97df19826bbf27542502eec84
kimjiae970 <kimjiae970@gmail.com>
2022-12-31 14:12:16 +0900

    22/12/31 알고리즘 문제 풀이

---
+++ src/Prac2.java
@@ -0,0 +1,18 @@
+class Prac2 {
+    public int solution(int[] numbers) {
+
+        int answer = 0;
+
+        for( int i = 0; i < 10; i++ ) {
+            answer += i ;
+        }
+
+        for ( int i = 0; i < numbers.length; i++) {
+
+            answer -= numbers[i];
+        }
+        System.out.println(answer);
+        return answer;
+    }
+
+}
