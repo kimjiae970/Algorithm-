cb1c00bb061acf0fbc26b10824bdf3d99748e721
kimjiae970 <kimjiae970@gmail.com>
2023-01-02 18:18:49 +0900

    23/01/02 알고리즘 문제 풀이

--- src/Prac3.java
+++ src/Prac3.java
@@ -1,2 +1,22 @@
+import java.util.Arrays;
+
 public class Prac3 {
+    public String solution(String s) {
+
+        String answer = "";
+
+        String[] arr = s.split(" ");
+
+        int[] arr1 = new int[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+           arr1[i] = Integer.parseInt(arr[i]);
+        }
+
+        Arrays.sort(arr1);
+
+        answer += (arr1[0] + " "+ arr1[arr1.length-1]);
+
+        return answer;
+    }
 }
