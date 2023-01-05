6c68161aaaa4a7f042dee0fb0940c3b2d78a2273
kimjiae970 <kimjiae970@gmail.com>
2023-01-05 22:59:18 +0900

    23/01/05 알고리즘 문제 풀이

---
+++ src/Prac5.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+class Prac5 {
+    public int[] solution(long n) {
+
+        String s = String.valueOf(n);
+
+        int[] answer = new int[s.length()];
+
+
+        for(int i = 0; i < s.length(); i++) {
+
+            answer[i] = Integer.parseInt(String.valueOf(s.charAt(s.length()-i-1)));
+        }
+
+        System.out.println(Arrays.toString(answer));
+        return answer;
+    }
+}
+
