43ff9566dc3379db6e4cd49b703e0642a581c282
kimjiae970 <kimjiae970@gmail.com>
2023-01-09 17:58:29 +0900

    23/01/09 알고리즘 문제 풀이

---
+++ src/Prac6.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+// 최솟값 만들기
+
+class Prac6 {
+        public int solution(int []A, int []B)
+        {
+            int answer = 0;
+
+            Arrays.sort(A);
+            Arrays.sort(B);
+
+            int[] arr = new int[B.length];
+
+            for(int i = 0; i < B.length; i++) {
+                arr[B.length-1-i] = B[i];
+            }
+
+            for(int i =0; i<A.length; i++) {
+                answer += A[i]*arr[i];
+            }
+
+            return answer;
+        }
+
+}
