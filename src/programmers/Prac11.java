74e0297fb8ef78471947ccabec52154796d3cc74
kimjiae970 <109570479+kimjiae970@users.noreply.github.com>
2023-01-30 15:43:15 +0900

    23/01/30 알고리즘 문제 풀이

---
+++ src/programmers/Prac11.java
@@ -0,0 +1,29 @@
+package programmers;
+
+import java.util.Stack;
+
+class Prac11 {
+
+    public static int solution(String s) {
+        Stack<Character> stack = new Stack<>();
+        int idx = 0;
+
+        if (s.length() == 0) {
+            return 0;
+        }
+
+        stack.push(s.charAt(idx++));
+        while (idx < s.length()) {
+            char c = s.charAt(idx++);
+
+            if (!stack.isEmpty() && stack.peek() == c) {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+
+        }
+
+        return stack.isEmpty() ? 1 : 0;
+    }
+}
