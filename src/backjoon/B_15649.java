77c36370d0917664ac5017f45307ab3a90ecd4ed
kimjiae970 <109570479+kimjiae970@users.noreply.github.com>
2023-03-06 23:58:39 +0900

    23/03/06 알고리즘 문제 풀이

--- src/backjoon/B_15649.java
+++ src/backjoon/B_15649.java
@@ -1,37 +1,49 @@
 package backjoon;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Scanner;
+import java.util.*;
 
 public class B_15649 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+
+        ArrayList<Integer> number = new ArrayList<>();
+        // Deque<Integer> deq = new ArrayDeque<>();
+
         int n = sc.nextInt();
         int m = sc.nextInt();
+        int firstNum = 1;
+        int count = 0;
+        int[] result = new int[m];
 
-       int[] arr = new int[m];
-
-       int z = 0;
-        int i = 0;
-        int j = 0;
-        int temp = 0;
+        for (int i = 1; i <= n; i++) {
+            number.add(i);
+        }
 
 
-        while (i < n) {
-            while (j < m) {
+        do {
 
-                for (int x = 1; x <= n; x++) {
-                    for (int y = 1; y <= n; y++) {
-
-                    }
-
-                }
-
+            for (int i = 0; i < m; i++) {
+                result[i] = number.get(i);
             }
 
 
-        }}
+            int temp = number.get(m - 1);
+            number.add(temp);
+            number.remove(m - 1);
+            count++;
+
+
+            if (result[m-1]==n && count == m) {
+                count = 0;
+                number.add(temp);
+                number.remove(m - 1);
+            }
+
+            System.out.println(Arrays.toString(result));
+            firstNum++;
+
+        } while (firstNum <= n);
+
     }
+}
