428bd9d1635d50c1195dfe38248899f664f77d41
kimjiae970 <109570479+kimjiae970@users.noreply.github.com>
2023-12-08 10:52:18 +0900

    23/12/8 알고리즘 문제 풀이 실패

--- src/programmers/Prac18.java
+++ src/programmers/Prac18.java
@@ -1,61 +1,57 @@
 package programmers;
 
+import java.sql.SQLOutput;
+import java.util.ArrayList;
 import java.util.HashSet;
 
 class Prac18 {
-        public boolean solution(String[] phone_book) {
-
-            boolean answer = true;
-
-            HashSet<String> set = new HashSet<>();
-
-            int i = 0;
+    public boolean solution(String[] p) {
 
 
+        boolean answer = true;
 
-            for( int j = 0; j < phone_book.length; j++) {
+        ArrayList<String> phone = new ArrayList<>();
+        boolean x = true;
 
-                set.clear();
-
-                for( int k = 0; k < phone_book.length; k++) {
-
-                    int length = phone_book[i].length();
-
-                    if(phone_book[k].length() < length) {
-                        continue;
-                    }else if (!phone_book[k].equals(phone_book[i])){
-                        if(phone_book[k].substring(0, length).equals(phone_book[i])){
-                            return false;
-                        }
-                    }else {
-                        set.add(phone_book[k].substring(0,length));
-                    }
-
-                }
-            }
-            while (i < phone_book.length) {
-
-                set.clear();
-
-                int length = phone_book[i].length();
-
-                for (String s : phone_book) {
-
-                   if(s.length() < length) {
-                        continue;
-                    } else if(s.substring(0, length).equals(phone_book[i]) && !s.equals(phone_book[i])){
-                        return false;
-                    } else {
-                        set.add(s.substring(0, length));
-                    }
-                }
-
-                i++;
-            }
-
-            System.out.println(answer);
-            return answer;
+        for(int i = 0; i < p.length; i++) {
+            phone.add(p[i]);
         }
+
+        for(int i = 0; i < phone.size(); i++) {
+            String Str1 = phone.get(i);
+            int len1 = phone.get(i).length();
+
+            for(int j = 0; j < phone.size(); j++) {
+
+              String Str2 = phone.get(j);
+              int len2 = phone.get(j).length();
+
+              if(Str1.equals(Str2)){
+                    continue;;
+              }else {
+                  if (Str1.length() > Str2.length()) {
+                      Str1.substring(0, Str2.length() - 1);
+
+                      if (Str1.equals(Str2)) {
+                          x = false;
+                      }
+              } else if (Str2.length() > Str1.length()) {
+                      Str2.substring(0, Str1.length() - 1);
+
+                      if (Str1.equals(Str2)) {
+                          x = false;
+                      }
+              } else if (Str1.equals(Str2)) {
+
+                          x = false;
+              }
+
+                  System.out.println(x);
+                }
+
+                }
+
+
 
 
     public static void main(String[] args) {
@@ -68,7 +64,7 @@ class Prac18 {
         String[] phon = {"123","456","789"};
         p.solution(phon);
 
-        String[] phone= {"12","123","1235","567","88"};
+        String[] phone1 = {"12","123","1235","567","88"};
         p.solution(phone);
 
         String[] phone_ = {"119", "114", "112", "123223123", "1231231234"};
